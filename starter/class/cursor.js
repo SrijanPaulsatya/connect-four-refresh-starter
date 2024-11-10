5c23673d59d2806a1ca098e7a6dc14b792708d74
Srijan Paulsatya <srijanpaulsatya@gmail.com>
2024-11-10 14:18:53 +0530

    Add Movement Logic

--- starter/class/cursor.js
+++ starter/class/cursor.js
@@ -24,10 +24,36 @@ class Cursor {
 
   left() {
     // Move cursor left
+    if (this.col === 0) {
+      this.col = 0;
+    } else {
+      this.col--;
+    }
   }
 
   right() {
     // Move cursor right
+    if (this.col === this.numCols - 1) {
+      this.col = this.numCols - 1;
+    } else {
+      this.col++;
+    }
+  }
+
+  up() {
+    if (this.row === 0) {
+      this.row = 0;
+    } else {
+      this.row--;
+    }
+  }
+
+  down() {
+    if (this.row === this.numRows - 1) {
+      this.row = this.numRows - 1;
+    } else {
+      this.row++;
+    }
   }
 
 }
